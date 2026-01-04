87ce2920d2491d7cf4484cf96fe24da37727e969
Sid0347 <siddharthgaikwad633@gmail.com>
2026-01-04 20:56:01 +0530

    Add argvs_validation function for command line argument validation

---
+++ argvs_validation.c
@@ -0,0 +1,13 @@
+#include "apc.h"
+
+/*............................................................................*/
+/*Title			    : argvs validation
+ Description	    : This function will validate all the argvs like argv count, 2nd or 4th argv is digits or not or 3rd is operator or not.
+ Prototype		    : int argvs_validation(int argc, char *argv[])
+ Input Parameters	: argc count and all CLA arguments.
+ Output			    : Status (SUCCESS / FAILURE)
+*/
+int argvs_validation(int argc, char *argv[])
+{
+    
+}
